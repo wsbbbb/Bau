a1f2fff1b26bd8811ac58a6cd5e1cb09cda3a306
wsbbbb <wsbstellar@outlook.com>
2024-06-04 16:36:07 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>Our website is coming soon</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no"/>
+	<meta name="wb_main" content="wb_element ebe508295ec19ec0cd7a6f0ba8067bd5f7e65a577e8bc8a3f021058ba70cb0e1" />
+	<style type="text/css">
+		html {
+			min-height: 100%;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+		}
+		body {
+			padding: 0;
+			margin: 0;
+			border: none;
+			font-size: 30px;
+			font-weight: normal;
+			font-family: Tahoma,Geneva,sans-serif;
+			line-height: 48px;
+			color: #F5EBE0;
+			text-align: center;
+			text-shadow: 2px 2px 2px rgba(0,0,0,0.3);
+			background-color: #4F9373;
+		}
+		.wb-content {
+			display: flex;
+			flex-direction: column;
+			justify-content: flex-start;
+			align-items: center;
+			margin: 0 40px;
+		}
+		.wb-hr {
+			display: block;
+			border-top: 4px solid #F5EBE0;
+			width: 100%;
+			max-width: 300px;
+			margin: 30px auto;
+			box-shadow: 2px 2px 2px rgba(0,0,0,0.3);
+		}
+		h1 {
+			display: block;
+			font-size: 80px;
+			font-weight: normal;
+			font-family: Tahoma,Geneva,sans-serif;
+			line-height: 80px;
+			margin: 20px 0px 20px 0;
+			padding: 0;
+		}
+		div.wb-domain {
+			display: block;
+			text-decoration: underline;
+		}
+		.wb-list {
+			display: block;
+			text-align: left;
+			margin: 100px 0;
+		}
+		.wb-list ol {
+			padding: 10px 0;
+			margin: 0 0 0 36px;
+			list-style-position: outside;
+		}
+		.wb-powered {
+			display: block;
+			margin-top: 20px;
+			font-size: 23px;
+			display: flex;
+			flex-direction: row;
+			justify-content: center;
+			align-items: center;
+		}
+		.wb-powered img {
+			margin-left: 10px;
+		}
+		.wb_main { display: none; }
+		@media (max-width: 991px) {
+			body {
+				font-size: 24px;
+				line-height: 38px;
+			}
+			h1 {
+				font-size: 64px;
+				line-height: 64px;
+			}
+			.wb-hr {
+				margin: 24px auto;
+			}
+			.wb-list {
+				margin: 80px 0;
+			}
+			.wb-list ol {
+				margin: 0 0 0 29px;
+			}
+			.wb-powered {
+				font-size: 18px;
+			}
+		}
+		@media (max-width: 767px) {
+			body {
+				font-size: 15px;
+				line-height: 24px;
+			}
+			h1 {
+				font-size: 40px;
+				line-height: 40px;
+			}
+			.wb-hr {
+				margin: 15px auto;
+			}
+			.wb-list {
+				margin: 50px 0;
+			}
+			.wb-list ol {
+				margin: 0 0 0 18px;
+			}
+			.wb-powered {
+				font-size: 12px;
+			}
+		}
+	</style>
+</head>
+<body>
+	<div class="wb-content">
+		<div class="wb-hr"></div>
+		<h1>Our website is coming soon</h1>
+		<div class="wb-domain">nusantara-stellar.com</div>
+		<div class="wb-list">
+			<div><b>Manage Website:</b></div>
+			<ol>
+							<li>Login to your server Control Panel</li>
+							<li>Click "Site Builder"</li>
+							<li>Choose Template. Edit Website</li>
+							<li>Publish</li>
+						</ol>
+		</div>
+		<div class="wb-hr"></div>
+		<div class="wb-powered">Powered by Site Builder</div>
+	</div>
+</body>
+</html>
